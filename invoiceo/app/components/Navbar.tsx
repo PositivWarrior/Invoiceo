be6a3b981105bef798fc7b67fa4daf5571ae5b37
Kacper Margol <kacpermargol@gmail.com>
2025-06-16 21:42:27 +0200

    Add Hero and Navbar components, enhance globals.css with animations and colors
    - Introduced new `Hero` and `Navbar` components to improve the homepage layout and user navigation.
    - Updated `globals.css` to include new color variables and a rainbow animation for enhanced UI effects.
    - Modified the main page structure to incorporate the new components, replacing the previous static content.

---
+++ invoiceo/app/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Logo from '@/public/logo_nobg.png';
+import { buttonVariants } from '@/components/ui/button';
+import { RainbowButton } from '@/components/magicui/rainbow-button';
+
+export default function Navbar() {
+	return (
+		<div className="flex items-center justify-between">
+			<Link href="/">
+				<Image src={Logo} alt="Logo" className="size-50" />
+			</Link>
+
+			<Link href="/login">
+				<RainbowButton>Get Started</RainbowButton>
+			</Link>
+		</div>
+	);
+}
