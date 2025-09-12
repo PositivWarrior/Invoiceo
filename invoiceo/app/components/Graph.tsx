bfea982a4344c3508121d56cbb7220aea473edb7
Kacper Margol <kacpermargol@gmail.com>
2025-09-12 11:06:56 +0200

    Refactor Graph component by removing unused import for improved code clarity

--- invoiceo/app/components/Graph.tsx
+++ invoiceo/app/components/Graph.tsx
@@ -5,7 +5,7 @@ import {
 	ChartTooltip,
 	ChartTooltipContent,
 } from '@/components/ui/chart';
-import { Line, LineChart, XAxis, YAxis, ResponsiveContainer } from 'recharts';
+import { Line, LineChart, XAxis, YAxis } from 'recharts';
 
 interface GraphProps {
 	data: {
